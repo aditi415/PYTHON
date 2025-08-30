279cc6b82efa4936c9963a2405d4c9c07b661652
Aditi Bhosure <bhosure.as2003@gmail.com>
2025-08-30 16:33:15 +0530

    Add files via upload

---
+++ 1stprog.py
@@ -0,0 +1,16 @@
+print("Hey..! Good morning\n", "Have a nice day")  #\n will print the further sentence on next line
+
+#-----------------------variables
+
+name ="Aditi"
+age=21
+salary=25000
+
+print(f"My name is {name} and I am {age} years old")
+
+print(type(name))                     #type descibe which type of data in present
+print(type(age))
+print(type(salary))
+
+a=None
+print(type(a))
